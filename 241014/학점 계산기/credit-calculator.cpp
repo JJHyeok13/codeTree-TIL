62fee6a6f217ebde6046060ef10cd9e2b69f2f68
SnowBlossom <116304804+JJHyeok13@users.noreply.github.com>
2024-10-14 03:48:09 +0900

    Title: 학점 계산기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241014/학점 계산기/credit-calculator.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    double sum = 0;
+    double input;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> input;
+        sum += input;
+    }
+
+    double average = round((sum / 4) * 10) / 10;
+
+    cout << average << endl;
+
+    if (average >= 4.0) {
+        cout << "Perfect";
+    }
+    else if (average < 3.0) {
+        cout << "Poor";
+    }
+    else {
+        cout << "Good";
+    }
+
+    return 0;
+}
