ac75bcf31f4c491c9642479bdef0c7eca64e67ae
SnowBlossom <116304804+JJHyeok13@users.noreply.github.com>
2024-10-14 03:35:44 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241014/원소 10개의 합/sum-of-10-elements.cpp
+++ 241014/원소 10개의 합/sum-of-10-elements.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    int input;
+    int sum = 0;
+
+    for (int i = 0; i < 10; i++) {
+        cin >> input;
+        sum += input;
+    }
+
+    cout << sum;
+    
     return 0;
 }
