dccd4c4030f6a94ad890d61a13bd0917724f1d8e
SnowBlossom <116304804+JJHyeok13@users.noreply.github.com>
2024-10-18 02:55:20 +0900

    Title: 일의 자리 배열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241018/일의 자리 배열/array-with-units.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    int n, m;
+
+    for (int i = 0; i < 2; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 2; i < 10; i++) {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 10;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
