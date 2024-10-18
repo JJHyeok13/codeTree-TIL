3bc6f32dca028c3f4a166d57d053b42f5bfcc04b
SnowBlossom <116304804+JJHyeok13@users.noreply.github.com>
2024-10-18 16:51:22 +0900

    Title: 특정 위치의 문자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241018/특정 위치의 문자/char-in-specific-location.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char arr[6] = {'L', 'E', 'B', 'R', 'O', 'S'};
+    char input;
+
+    cin >> input;
+
+    for (int i = 0; i < 6; i++) {
+        if (arr[i] == input) {
+            cout << i;
+            return 0;
+        }
+    }
+
+    cout << "None";
+
+    return 0;
+}
