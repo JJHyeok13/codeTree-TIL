2e1083fb32f97fbfaae4267b493484750bd62bf5
SnowBlossom <116304804+JJHyeok13@users.noreply.github.com>
2024-10-23 01:15:18 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241023/체크판위에서 2/on-the-checkboard-2.cpp
+++ 241023/체크판위에서 2/on-the-checkboard-2.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 int main() {
     int R, C;
-    char arr[16][16];
+    char arr[15][15];
     int answer = 0;
 
     cin >> R >> C;
 
-    for (int i = 1; i <= R; i++) {
-        for (int j = 1; j <= C; j++) {
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
             cin >> arr[i][j];
         }
     }
 
-    for (int i = 1; i <= R - 1; i++) {
-        for (int j = 1; j <= C - 1; j++) {
-            for (int l = i + 1; l <= R - 2; l++) {
-                for (int k = j + 1; k <= C - 2; k++) {
-                    if (arr[1][1] != arr[i][j] &&
+    for (int i = 1; i < R; i++) {
+        for (int j = 1; j < C; j++) {
+            for (int l = i + 1; l < R - 1; l++) {
+                for (int k = j + 1; k < C - 1; k++) {
+                    if (arr[0][0] != arr[i][j] &&
                         arr[i][j] != arr[l][k] &&
-                        arr[l][k] != arr[R][C]) {
+                        arr[l][k] != arr[R - 1][C - 1]) {
                         answer++;
                     }
                 }
