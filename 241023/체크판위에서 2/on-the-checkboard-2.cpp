f65dfd4fb2db0671007cb26d42ac2bf16d53a1aa
SnowBlossom <116304804+JJHyeok13@users.noreply.github.com>
2024-10-23 01:11:08 +0900

    Title: 체크판위에서 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241023/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int R, C;
+    char arr[16][16];
+    int answer = 0;
+
+    cin >> R >> C;
+
+    for (int i = 1; i <= R; i++) {
+        for (int j = 1; j <= C; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 1; i <= R - 1; i++) {
+        for (int j = 1; j <= C - 1; j++) {
+            for (int l = i + 1; l <= R - 2; l++) {
+                for (int k = j + 1; k <= C - 2; k++) {
+                    if (arr[0][0] != arr[i][j] &&
+                        arr[i][j] != arr[l][k] &&
+                        arr[l][k] != arr[16][16]) {
+                        answer++;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
